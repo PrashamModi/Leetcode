cac2cca227096dc56a74d3816ed188acaa5f8e69
PrashamJodd <109425729+PrashamModi@users.noreply.github.com>
2023-09-24 16:22:27 +0530

    Time: 13 ms (32.38%), Space: 11.2 MB (62.19%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.cpp
+++ 0815-champagne-tower/0815-champagne-tower.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
-    double f(int poured, int i, int j, vector<vector<double>> &dp){
-        if(i < 0 || j < 0 || i < j){
-            return 0.0;
-        }
+    // double f(int poured, int i, int j, vector<vector<double>> &dp){
+    //     if(i < 0 || j < 0 || i < j){
+    //         return 0.0;
+    //     }
 
-        if(i == 0 && j == 0){
-            return poured;
-        }
+    //     if(i == 0 && j == 0){
+    //         return poured;
+    //     }
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
         
-        double ul = (f(poured, i - 1, j - 1, dp) - 1) / 2.0;
-        double ur = (f(poured, i - 1, j, dp) - 1) / 2.0;
+    //     double ul = (f(poured, i - 1, j - 1, dp) - 1) / 2.0;
+    //     double ur = (f(poured, i - 1, j, dp) - 1) / 2.0;
 
-        ul = max(ul, 0.0);
-        ur = max(ur, 0.0);
+    //     ul = max(ul, 0.0);
+    //     ur = max(ur, 0.0);
 
-        return dp[i][j] = ul + ur;
-    }
+    //     return dp[i][j] = ul + ur;
+    // }
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<vector<double>> dp(query_row + 1, vector<double> (query_glass + 1, -1));
-        return min(1.0, f(poured, query_row, query_glass, dp));
+        vector<vector<double>> dp(query_row + 1, vector<double> (query_glass + 1, 0));
+
+        dp[0][0] = (double)poured;
+
+        for(int i = 1; i <= query_row; i++){
+            for(int j = 0; j <= query_glass; j++){
+                double ul = 0.0;
+                if(j > 0) ul = (dp[i - 1][j - 1] - 1) / 2.0;
+                double ur = (dp[i - 1][j] - 1) / 2.0;
+
+                ul = max(ul, 0.0);
+                ur = max(ur, 0.0);
+
+                dp[i][j] = ul + ur;
+            }
+        }
+        return min(1.0, dp[query_row][query_glass]);
     }
 };
