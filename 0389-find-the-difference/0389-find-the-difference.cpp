05e536a2b584262096f6005613bf64081eda0b50
PrashamJodd <109425729+PrashamModi@users.noreply.github.com>
2023-09-25 14:51:02 +0530

    Time: 0 ms (100%), Space: 7.1 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> v(26, 0);
-        for(int i = 0; i < s.size(); i++){
-            v[s[i] - 'a']++;
-        }
-        for(int j = 0; j < t.size(); j++){
-            v[t[j] - 'a']++;
-        }
-        for(int i = 0; i < 26; i++){
-            if(v[i] % 2 == 1){
-                char ans = i + 'a';
-                return ans;
-            }
-        }
-        return '123';
 
-        
+        int sum = 0;
+        for(auto it : t)sum += it;
+        for(auto it : s)sum -= it;
+        return (char)(sum);
+
+        // char ans = '\0';
+        // for(auto it : s) ans ^= it;
+        // for(auto it : t) ans ^= it;
+        // return ans;
+
+
+        // vector<int> v(26, 0);
+        // for(int i = 0; i < s.size(); i++){
+        //     v[s[i] - 'a']++;
+        // }
+        // for(int j = 0; j < t.size(); j++){
+        //     v[t[j] - 'a']++;
+        // }
+        // for(int i = 0; i < 26; i++){
+        //     if(v[i] % 2 == 1){
+        //         char ans = i + 'a';
+        //         return ans;
+        //     }
+        // }
+        // return '123';
     }
 };
