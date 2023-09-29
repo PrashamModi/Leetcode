9d37189037e8e46f45935ac04ccb0bd9a7e0f0fe
PrashamJodd <109425729+PrashamModi@users.noreply.github.com>
2023-09-29 12:42:20 +0530

    Time: 496 ms (11.22%), Space: 29.7 MB (25.71%) - LeetHub

--- 0923-super-egg-drop/0923-super-egg-drop.cpp
+++ 0923-super-egg-drop/0923-super-egg-drop.cpp
@@ -28,7 +28,40 @@ public:
         return dp[eggs][floors] = ans;
     }
     int superEggDrop(int k, int n) {
-        vector<vector<int>> dp(k + 1, vector<int>(n + 1, -1));
-        return f(k, n, dp);
+        vector<vector<int>> dp(k + 1, vector<int>(n + 1, 0));
+
+        for(int i = 0; i <= n; i++){
+            dp[1][i] = i; // if egg is 1 
+        }
+
+        for(int i = 0; i <= k; i++){
+            dp[i][1] = 1; //if floor is 1
+        }
+
+        for(int eggs = 2; eggs <= k; eggs++){
+            for(int floors = 2; floors <= n; floors++){
+                int ans = 1e7;
+                int low = 0, high = floors;
+
+                while(low <= high) {
+                    int mid = low + (high - low)/2;
+                    int left = dp[eggs - 1][mid - 1];
+                    int right = dp[eggs][floors - mid];
+
+                    int worstCase = 1 + max(left, right);
+
+                    if(left < right){
+                        low = mid + 1;
+                    } else{
+                        high = mid - 1;
+                    }
+
+                    ans = min(ans, worstCase);
+                }
+                dp[eggs][floors] = ans;
+            }
+        }
+
+        return dp[k][n];
     }
 };
