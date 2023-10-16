01135d594a183b3f6e07cfce5340cc6fe39c22e0
PrashamJodd <109425729+PrashamModi@users.noreply.github.com>
2023-10-16 15:18:32 +0530

    Time: 299 ms (45.77%), Space: 153.6 MB (42.71%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    long long f(int ind, vector<int> &nums, int isEven, vector<vector<long long>> &dp) {
-        if(ind >= nums.size()) return 0;
-
-        if(dp[ind][isEven] != -1) return dp[ind][isEven];
-
-        if(isEven) {
-            long long notTake = f(ind + 1, nums, isEven, dp);
-            long long take = nums[ind] + f(ind + 1, nums, !isEven, dp);
-            return dp[ind][isEven] = max(take,  notTake);
-        } else {
-            long long notTake = f(ind + 1, nums, isEven, dp);
-            long long take = -nums[ind] + f(ind + 1, nums, !isEven, dp);
-            return dp[ind][isEven] = max(take, notTake);
-        }
-    }
+    
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<long long>> dp(n, vector<long long>(2, -1));
-        return f(0, nums, 1, dp);
+        vector<vector<long long>> dp(n + 1, vector<long long>(2, 0));
+        for(long long ind = n - 1; ind >= 0; ind--) {
+            for(int isEven = 0; isEven <= 1; isEven++) {
+                if(isEven) {
+                    long long notTake = dp[ind + 1][isEven];
+                    long long take = nums[ind] + dp[ind + 1][!isEven];
+                    dp[ind][isEven] = max(take,  notTake);
+                } else {
+                    long long notTake = dp[ind + 1][isEven];
+                    long long take = -nums[ind] + dp[ind + 1][!isEven];
+                    dp[ind][isEven] = max(take, notTake);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
